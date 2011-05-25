91105acfb36d9ef4e4abde89692f40f2e1d6709d
Mats Becker <mats.becker@gmail.com>
2011-05-25 18:38:40 +0200

    Kamerabild wird alle 11 Sek neu abgerufen

--- myHome iPhone/myHome iPhone/Resources/js/menue_kamera.js
+++ myHome iPhone/myHome iPhone/Resources/js/menue_kamera.js
@@ -34,34 +34,41 @@ var suds = new SudsClient({
 	targetNamespace: Titanium.App.Properties.getString('url')
 });
 
-try {
-    suds.invoke('getSnapshot', callparams, function(xmlDoc) {
-        var results = xmlDoc.documentElement.getElementsByTagName('return');
-        if (results && results.length>0) {
-            
-			for(var n = 0; n < results.length; n++){
-				
-				var result = results.item(n);							
-				var image = result.getElementsByTagName('image').item(0).text;
-				
-				var imagewrite = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, 'kamera.png');
-                var imageDecode = Titanium.Utils.base64decode(image);
-				imagewrite.write( imageDecode );
-				Titanium.API.info("Writing image: " + imagewrite.nativePath);
-				
-				win1.backgroundImage = imagewrite.nativePath;
-							
-			} // for(var n = 0; n < results.length; n++)
-						
-					
-        } else {
-            Titanium.API.info('Error: SOAP call.');
-        }
-		
-    }); // suds.invoke
-} catch(e) {
-    Ti.API.error('Error: ' + e);
+function getCameraImage(){
+	try {
+	    suds.invoke('getSnapshot', callparams, function(xmlDoc) {
+	        var results = xmlDoc.documentElement.getElementsByTagName('return');
+	        if (results && results.length>0) {
+
+				for(var n = 0; n < results.length; n++){
+
+					var result = results.item(n);							
+					var image = result.getElementsByTagName('image').item(0).text;
+
+					var imagewrite = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, 'kamera.png');
+	                var imageDecode = Titanium.Utils.base64decode(image);
+					imagewrite.write( imageDecode );
+					Titanium.API.info("Writing image: " + imagewrite.nativePath);
+
+					win1.backgroundImage = imagewrite.nativePath;
+
+				} // for(var n = 0; n < results.length; n++)
+
+
+	        } else {
+	            Titanium.API.info('Error: SOAP call.');
+	        }
+
+	    }); // suds.invoke
+	} catch(e) {
+	    Ti.API.error('Error: ' + e);
+	}
 }
+// Erzeuge Kamerabild beim Aufruf des Fensters
+getCameraImage();
+// Erzeuge alle 11 Sek. ein neues Kamerabild
+setInterval(getCameraImage, 11000);
+
 
 
 var logoutBtn = Titanium.UI.createButton({
